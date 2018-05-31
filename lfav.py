e332891d22f4caa93d1551e8f4ca76ed2f093e9b
Justin Lin <justinwlin@gmail.com>
2018-05-30 23:34:14 -0700

    updating battle

--- lfav.py
+++ lfav.py
@@ -2,24 +2,26 @@ import csv
 import copy
 import random
 
-#Global Variables:
+# Global Variables:
 unitList = []
-STATS = ["LVL", "HP", "STR", "MAG", "SPD", "SKL", "LCK", "DEF", "RES"]
+STATS = ["Lvl", "HP", "Str", "Mag", "Spd", "Skl", "Lck", "Def", "Res"]
+
+
 class Unit:
     def __init__(self, list):
         self.name = list[0]
-        self.equipped_weapon = list[1] #allowing this to be the change to stat
+        self.equipped_weapon = list[1]
         self.weapon_mastery = list[1]
         '''
         ================
-        Base Stats Initialization
+        Stat Initialization
         ================
         '''
         self.base = {}
         self.cap = {}
         self.current = {}
         self.growth = {}
-        #Initializing Base/cap/growth/current
+        # Initializing base/cap/growth/current
         for i in range(0, 9):
             self.base[STATS[i]] = int(list[i + 2])
             self.cap[STATS[i]] = int(list[i + 11])
@@ -31,53 +33,63 @@ class Unit:
         Battle Simulation Stats
         ================
         '''
-        #Weapon Adding to Attack:
-        self.attack = self.base["STR"]
-        self.accuracy = self.base["SPD"] + 2 * self.base["SKL"]
-        self.avoid = self.base["LCK"] + .5 * self.base["SKL"]
-        self.crit = .5 * self.base["SKL"]
-        self.dodge = 2 * self.base["SPD"] + .5 * self.base["SKL"] + .5 * self.base["LCK"]
-    
+        self.attack = self.current["Str"]
+        self.accuracy = .5 * self.current["Spd"] + 2 * self.current["Skl"]
+        self.avoid = self.current["Lck"] + .5 * self.current["Skl"]
+        self.crit = .5 * self.current["Skl"]
+        self.dodge = 2 * self.current["Spd"] + .5 * self.current["Skl"] + .5 * self.current["Lck"]
+
+    def setBattleStatToBase(self):
+        self.attack = self.current["Str"]
+        self.accuracy = .5 * self.current["Spd"] + 2 * self.current["Skl"]
+        self.avoid = self.current["Lck"] + .5 * self.current["Skl"]
+        self.crit = .5 * self.current["Skl"]
+        self.dodge = 2 * self.current["Spd"] + .5 * self.current["Skl"] + .5 * self.current["Lck"]
+
     def printBattleStat(self):
-        print("ATTACK" + ": " + str(self.attack))
-        print("ACCURACY" + ": " + str(self.accuracy))
-        print("AVOID" + ": " + str(self.avoid))
-        print("CRIT" + ": " + str(self.crit))
-        print("DODGE" + ": " + str(self.dodge))
+        print("Attack: " + str(self.attack))
+        print("Accuracy: " + str(self.accuracy))
+        print("Dodge: " + str(self.dodge))
+        print("Critical: " + str(self.crit))
+        print("Avoid: " + str(self.avoid))
 
     def battleStatWeapon(self, weaponChoice):
-        #Resetting to Base stats
-        self.attack = self.base["STR"]
-        self.accuracy = self.base["SPD"] + 2 * self.base["SKL"]
-        self.avoid = self.base["LCK"] + .5 * self.base["SKL"]
-        self.crit = .5 * self.base["SKL"]
-        self.dodge = 2 * self.base["SPD"] + .5 * self.base["SKL"] + .5 * self.base["LCK"]
-
-        #With Weapons
+        # Resetting to base stats
+        self.setBattleStatToBase()
+        # With weapons
         self.equipped_weapon = weaponChoice
         if(self.equipped_weapon == "iron"):
+            print("Iron weapon has been equipped.")
+            print("Attack: " + str(self.attack) + " -> " + str(self.attack + 6))
             self.attack = self.attack + 6
+            print("Accuracy: " + str(self.accuracy) + " -> " + str(self.accuracy + 85))
             self.accuracy = self.accuracy + 85
         elif (self.equipped_weapon == "steel"):
+            print("Steel weapon has been equipped.")
+            print("Attack: " + str(self.attack) + " -> "+ str(self.attack + 11))
             self.attack = self.attack + 11
+            print("Accuracy: " + str(self.accuracy) + " -> " + str(self.accuracy + 75))
             self.accuracy = self.accuracy + 75
         else:
-            print("The attached weapon has no recorded value so your attack will be sent to your character base strength.")
+            print("The attached weapon has no recorded value, so your attack will be set to your character's base strength.")
             self.attack = self.attack
     
+    def printBattleStatAttacking(self):
+        print("Attack: " + str(self.attack))
+        print("Accuracy: " + str(self.accuracy))
+        print("Critical: " + str(self.crit))
+    
+    def printBattleStatDefending(self):
+        print("Dodge: " + str(self.dodge))
+        print("Avoid: " + str(self.avoid))
+
     def battleStatCustom(self, might, accuracy):
+        # Base
+        self.setBattleStatToBase()
 
-        #Base
-        self.attack = self.base["STR"]
-        self.accuracy = self.base["SPD"] + 2 * self.base["SKL"]
-        self.avoid = self.base["LCK"] + .5 * self.base["SKL"]
-        self.crit = .5 * self.base["SKL"]
-        self.dodge = 2 * self.base["SPD"] + .5 * self.base["SKL"] + .5 * self.base["LCK"]
-
-        #With Weapon
+        # With Weapon
         self.attack = self.attack + might
         self.accuracy = self.accuracy + accuracy
-
 
     def getName(self):
         return self.name
@@ -96,38 +108,40 @@ class Unit:
 
     def printCap(self):
         for i in range(0, len(STATS)):
-            print(STATS[i] + " CAP: " + str(self.cap[STATS[i]]))
-    
+            print(STATS[i] + " Cap: " + str(self.cap[STATS[i]]))
+
     def printCurrentNCap(self):
         for i in range(0, len(STATS)):
-            print(STATS[i] + ": " + str(self.current[STATS[i]]) + "; " + STATS[i] + " Cap: " + str(self.cap[STATS[i]]))
+            print(STATS[i] + ": " + str(self.current[STATS[i]]) +
+                  "; " + STATS[i] + " Cap: " + str(self.cap[STATS[i]]))
 
     def avgStat(self, lvlInc):
         for i in range(0, len(STATS)):
-            print(STATS[i] + " Avg: "+ str("%.2f" % ((lvlInc * self.growth[STATS[i]]/100) + self.base[STATS[i]])) + " ; " + STATS[i] +  " Cap:  " + str(self.cap[STATS[i]]))
+            print(STATS[i] + " Avg: " + str("%.2f" % ((lvlInc * self.growth[STATS[i]]/100) +
+                                                      self.base[STATS[i]])) + " ; " + STATS[i] + " Cap:  " + str(self.cap[STATS[i]]))
 
     def setToCap(self):
         self.current = copy.deepcopy(self.cap)
 
     def chgStats(self, variableName, num):
         print(str(self.current[variableName]))
-    
+
     def levelSimulate(self, levels):
-        #Helper Function:
+        # Helper Function:
         def helpGrow(statGrowth):
-            randomNum = random.randint(1,100)
+            randomNum = random.randint(1, 100)
             if randomNum <= self.growth[statGrowth] and self.current[statGrowth] < self.cap[statGrowth]:
                 self.current[statGrowth] = self.current[statGrowth] + 1
                 print(statGrowth + " GROWTH!")
                 if self.current[statGrowth] == self.cap[statGrowth]:
                     print("Capped " + statGrowth + "!")
-            elif randomNum <= self.growth [statGrowth] and self.current[statGrowth] == self.cap[statGrowth]:
+            elif randomNum <= self.growth[statGrowth] and self.current[statGrowth] == self.cap[statGrowth]:
                 print("OVER " + statGrowth + "!")
         print("Starting Base Stats:")
         self.printCurrentNCap()
         for i in range(1, levels + 1):
             print("Level Simulation: " + str(i))
-            self.current['LVL'] = self.current['LVL'] + 1
+            self.current['Lvl'] = self.current['Lvl'] + 1
             for i in range(1, len(STATS)):
                 helpGrow(STATS[i])
         print("\n======================================")
@@ -141,11 +155,13 @@ BackEnd Functions
 ============================
 '''
 
+
 def listingUnit():
     for unit in unitList:
         print(unit.getName(), end="; ")
     print()
     print("====================")
+
 
 def searchForUnit(list, unitName):
     for i in list:
@@ -154,32 +170,34 @@ def searchForUnit(list, unitName):
     else:
         return("Error")
 
+
 def battle_(unit1, unit2):
     unit1Accuracy = unit1.accuracy - unit2.dodge
     unit1Crit = unit1.crit - unit2.avoid
     crit = False
-    if(unit1Accuracy >= random.randint(1, 100)): #Testing if unit gets hit
+    if(unit1Accuracy >= random.randint(1, 100)):  # Testing if unit gets hit
         print(unit1.getName() + " has hit " + unit2.getName())
-        #Testing Crit
+        # Testing Crit
         if(unit1Crit >= random.randint(1, 100)):
-            print(unit1.getName() + " has crit " + unit2.getName() + "!!!")
+            print(unit1.getName() + " has crit!")
             crit = True
-        #Damage Dealt
-        damage = unit1.attack - unit2.current["DEF"]
-        #If Crit *3 to damage. 
+        # Damage Dealt
+        damage = unit1.attack - unit2.current["Def"]
+        # If Crit *3 to damage.
         if(crit):
             damage = damage * 3
-        #Subtracting Health
+        # Subtracting Health
         unit2.current["HP"] = unit2.current["HP"] - damage
 
         if(unit2.current["HP"] < 0):
             unit2.current["HP"] = 0
 
-        print(unit2.getName() + " has taken " + str(damage) + "!")
-        print(unit2.getName() + " current health is " + str(unit2.current["HP"]))
+        print(unit2.getName() + " has taken " + str(damage) + " damage!")
+        print(unit2.getName() + "'s current health is " +
+              str(unit2.current["HP"]) + ".")
     else:
-        print("ATTACK MISSED!")
-    
+        print("Attack missed!")
+
 
 '''
 ===========================
@@ -187,13 +205,14 @@ Battle Menus
 ============================
 '''
 
+
 def battleSimulationMenu():
     unitSuccessSelected = False
 
     unit1 = False
     unit2 = False
 
-    print("Select which units that you would like to do battle?")
+    print("Select the first unit to initiate battle.")
 
     while (not unitSuccessSelected):
         listingUnit()
@@ -203,24 +222,25 @@ def battleSimulationMenu():
             print(selectedUnit.name + " has been selected.")
             if(unit1 == False):
                 unit1 = selectedUnit
-                print("Please choose another unit for battle")
+                print("Select which unit to attack.")
             else:
                 unit2 = selectedUnit
                 unitSuccessSelected = True
         else:
-            print("Your unit was mispelled or not found please enter a proper input")
+            print("The unit was not found.")
     battleSimulate(unit1, unit2)
+
 
 def weaponSelect(unit):
     running = True
     while(running):
-        #MENU OPTION
-        print("Item Choice")
-        print("Select Weapon")
+        # MENU OPTION
+        print("Item choice")
+        print("Select weapon:")
         print("[1] Iron")
         print("[2] Steel")
         print("[3] Custom")
-        #LOGIC
+        # LOGIC
         menuSelection = int(input())
         if menuSelection == 1:
             unit.battleStatWeapon("iron")
@@ -229,9 +249,9 @@ def weaponSelect(unit):
             unit.battleStatWeapon("steel")
             running = False
         elif menuSelection == 3:
-            print("Please choose a given might for your weapon")
+            print("Please enter the weapon might.")
             might = int(input())
-            print("Please give your weapon an accuracy")
+            print("Please enter the weapon accuracy.")
             accuracy = int(input())
             unit.battleStatCustom(might, accuracy)
             running = False
@@ -240,18 +260,57 @@ def weaponSelect(unit):
 
 
 def battleSimulate(unit1, unit2):
-    #Battle Loop
+    # Battle Loop
     battle = True
     unit1Turn = True
+    
+    firstTime = True
+    storedUnit1 = 0
+    storedUnit2 = 0
+
     while(battle):
+        print("[1] Heal [2] Quit; Press [Enter] to continue normally")
+        option = int(input())
+        if(option == 1):
+            print("Which unit would you like to heal?")
+            unitSelected = input()
+            if(unitSelected == unit1.name):
+                unitSelected = unit1
+            else:
+                unitedSelected = unit2
+            print("[1] 10 HP; [2] 20 HP [3] 40 HP [4] Custom [5] Max")
+            hp = int(input)
+            if(hp == 1):
+                unitSelected.current["HP"] += 10
+            elif(hp == 2):
+                unitSelected.current["HP"] += 20
+            elif(hp == 3):
+                unitSelected.current["HP"] += 40
+            elif(hp == 4):
+                print("Enter an HP Value to heal by")
+                hpCustom = int(input())
+                unitSelected.current["HP"] += hpCustom
+            elif(hp == 5):
+                if(unitSelected.name == storedUnit1.name):
+                    unitSelected.current["HP"] = storedUnit1.current["HP"]
+                else:
+                    unitedSelected.current["HP"] = storedUnit2.current["HP"]
+            else:
+                print("None of your inputs was recognized, so onwards!")
+            
+        elif(option == 2):
+            break;
+        else:
+            print("ONTO BATTLE!")
+        
         if(unit1.current["HP"] <= 0 and unit2.current["HP"] <= 0):
-            print("Both units have lost their HP!")
+            print("Both units have been defeated!")
             battle = False
         elif(unit1.current["HP"] <= 0):
-            print(unit1.getName() + " has lost all HP!")
+            print(unit1.getName() + " has been defeated!")
             battle = False
         elif unit2.current["HP"] <= 0:
-            print(unit2.getName() + " has lost all HP!")
+            print(unit2.getName() + " has been defeated!")
             battle = False
         else:
             attackerName = unit1.name
@@ -262,23 +321,38 @@ def battleSimulate(unit1, unit2):
 
             attackerUnit = searchForUnit(unitList, attackerName)
             defenseUnit = searchForUnit(unitList, defenderName)
-            
-            print(attackerName + " is attacking " + defenderName)
-            print("What weapon do you want the attacker to use?")
+
+            if(firstTime):
+                storedUnit1 = copy.deepcopy(attackerUnit)
+                storedUnit2 = copy.deepcopy(defenseUnit)
+                firstTime = False
+
+            print(attackerName + " is attacking " + defenderName + ".")
+            print("What weapon should " + attackerName + " use?")
             weaponSelect(attackerUnit)
 
             unit1Turn = not unit1Turn
 
-            print("Stat of Current Unit")
+            print("Stats of current unit")
             print(attackerUnit.name)
             attackerUnit.printCurrent()
-            attackerUnit.printBattleStat()
+            attackerUnit.printBattleStatAttacking()
             print()
             print(defenseUnit.name)
             defenseUnit.printCurrent()
-            defenseUnit.printBattleStat()
+            defenseUnit.printBattleStatDefending()
             print()
             battle_(attackerUnit, defenseUnit)
+    print("Would you like to heal both characters? Type in 'Y'")
+    yes = input()
+    if(yes == "Y" or yes == "y"):
+        attackerUnit = storedUnit1
+        defenseUnit = storedUnit2
+
+        print("Units have been restored")
+        attackerUnit.printCurrent()
+        defenseUnit.printCurrent()
+
 
 '''
 ===========================
@@ -286,17 +360,18 @@ Menu System Regular
 ============================
 '''
 
+
 def menu():
     running = True
     while(running):
-        #MENU OPTION
+        # MENU OPTION
         print("Menu")
         print("Enter the number corresponding to the action you would like to take.")
         print("[1] Select a unit to work with.")
         print("[2] List all units again.")
-        print("[3] Battle Simulation")
+        print("[3] Initiate battle.")
         print("[0] Exit")
-        #LOGIC
+        # LOGIC
         menuSelection = int(input())
         if menuSelection == 0:
             running = False
@@ -363,6 +438,7 @@ def selectingUnit():
                     newUnit = copy.deepcopy(selectedUnit)
                     newUnit.name = newname
                     selectedUnit.current = copy.deepcopy(selectedUnit.base)
+                    newUnit.setBattleStatToBase()
                     unitList.append(newUnit)
                     print("The unit has been saved.")
                 else:
@@ -384,8 +460,8 @@ def selectingUnit():
                 listingUnit()
             else:
                 print("Please choose from the provided options.")
-    
-    #User Interface
+
+    # User Interface
     print("Which unit you would like to work with?")
     unitName = input()
     selectedUnit = searchForUnit(unitList, unitName)
@@ -393,23 +469,29 @@ def selectingUnit():
         print(selectedUnit.name + " has been selected.")
         menuSingleUnit()
     else:
-        print("Your unit was mispelled or not found")
+        print("Your unit was not found.")
         return
+
+
 '''
 ===========================
 Init and Main
 ============================
 '''
 
+
 def initFile():
-    #Initializes the file
+    # Initializes the file
     with open('stats.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             tempUnit = Unit(row)
             unitList.append(tempUnit)
+
+
 def main():
     initFile()
     menu()
+
 
 main()
